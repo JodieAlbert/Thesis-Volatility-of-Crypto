7d95878ec1aa6d8ace34012e88e9f853a89fe6a3
Jodie Albert <jodie.albert@mail.concordia.ca>
2022-02-09 23:21:07 -0500

    Linear models

--- Volatility.R
+++ Volatility.R
@@ -18,6 +18,7 @@ r[,ym := as.yearmon(date)]
 r[,r := c(NA,diff(log(price)))]
 
 d = r[, sd(r,na.rm=T), by = ym]
+colnames(d) <- c("ym", "VBTC")
 
 ##ETHEREUM
 getSymbols("ETH-USD")
@@ -35,6 +36,7 @@ r.ETH[,ym := as.yearmon(date)]
 r.ETH[,r.ETH := c(NA,diff(log(price)))]
 
 d.ETH = r.ETH[, sd(r.ETH,na.rm=T), by = ym]
+colnames(d.ETH) <- c("ym", "VETH")
 
 ##BINANCE
 getSymbols("BNB-USD")
@@ -52,6 +54,41 @@ r.BNB[,ym := as.yearmon(date)]
 r.BNB[,r.BNB := c(NA,diff(log(price)))]
 
 d.BNB = r.BNB[, sd(r.BNB,na.rm=T), by = ym]
+colnames(d.BNB) <- c("ym", "VBNB")
+
+###S&P500
+getSymbols("^GSPC")
+
+SP500= `GSPC`[ , "GSPC.Adjusted", drop=F]
+plot(SP500$GSPC.Adjusted)
+
+r.SP500 = data.frame(date = time(SP500), price = as.numeric(SP500$GSPC.Adjusted))
+
+setDT(r.SP500)
+
+r.SP500[,ym := as.yearmon(date)]
+
+r.SP500[,r.SP500 := c(NA,diff(log(price)))]
+
+d.SP500 = r.SP500[, sd(r.SP500,na.rm=T), by = ym]
+colnames(d.SP500) <- c("ym", "VSP")
+###GOLD
 
 
+##Merge
+Dpartial = d.BNB[d, on = .(ym)]
+D_2= d.ETH[Dpartial, on = .(ym)]
+Volatilitydata= d.SP500[D_2, on = .(ym)]
+class(as.data.frame(Volatilitydata))
+##linear model 
+library(tidyverse)
+
+# Multiple Linear Regression Example
+fit <- lm(VBTC ~ VBNB+ VETH+ VSP, data=Volatilitydata)
+fit2 <- lm(VBNB ~ VBTC+ VETH+ VSP, data=Volatilitydata)
+fit3 <- lm(VETH ~ VBTC+ VBNB+ VSP, data=Volatilitydata)
+summary(fit) # show results
+summary(fit2)
+summary(fit3)
+plot(fit)
 
