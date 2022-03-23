1d453f2681b3f59fd2fe12d5dcacb8ff9dc6ee39
Jodie Albert <jodie.albert@mail.concordia.ca>
2022-03-23 11:22:23 -0400

    Update weekly gtrends.R

--- weekly gtrends.R
+++ weekly gtrends.R
@@ -2,14 +2,14 @@ library(gtrendsR)
 library(dplyr)
 
 g2 <- gtrends(c("bitcoin","VIX","crisis","'cyber attack'","'gold price'")
-              ,time ="2012-06-24 2017-06-18")$interest_over_time
+              ,time ="2011-08-01 2022-01-31")$interest_over_time
 g2$week <- week(g2$date)
 g2$yw <- format(g2$date, '%Y-%V')
 
 
 
 g3 <- gtrends(c("'interest rate rise'","inflation","'stock market crash'","war")
-              ,time ="2012-06-24 2017-06-18")$interest_over_time
+              ,time ="2011-08-01 2022-01-31")$interest_over_time
 g3$week <- week(g3$date)
 g3$yw <- format(g3$date, '%Y-%V')
 
@@ -27,7 +27,7 @@ intg <- intgtrend1[ , .(weekly_freq = sum(hits)), by = isoweek(date)]
 
 
 intgtrend <- gtrends(c("'interest rate rise'")
-              ,time ="2012-06-24 2017-06-18")$interest_over_time
+              ,time ="2011-08-01 2022-01-31")$interest_over_time
 as.POSIXlt(intgtrend$date, format = "%Y %U %u")
 intgtrend$yw <- format(intgtrend$date, '%Y-%V')
 
